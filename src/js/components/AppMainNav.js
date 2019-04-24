99dce924a863692d6ac56d0898fc082a3ee4a44a
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-04-24 10:49:50 -0400

    performed light refactoring menu toggle logic

--- src/js/components/AppMainNav.js
+++ src/js/components/AppMainNav.js
@@ -4,7 +4,7 @@ import React from 'react';
 class AppMainNav extends React.Component {
   render() {
     return (
-      <div className={this.props.menuStatus ? '' : 'showMenu'}>
+      <div className="sight-reader-layout-nav" id={this.props.menuStatus ? 'isHidden' : ''}>
         <ul>
           <li>item 1</li>
           <li>item 2</li>
